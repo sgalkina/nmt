65de1e86659ff29eff1cae3ae87383e93fffb088
nikolaiknielsen <47968541+nikolaiknielsen@users.noreply.github.com>
2024-03-06 20:46:39 +0100

    Add files via upload

--- datapre/train_model.sh
+++ datapre/train_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p gpu                      # The partition/queue to run on, 'gpu' is standard
 #SBATCH --gres=gpu:1                # Request GPU resources
-#SBATCH --job-name=moflow-training  # Name of the job
+#SBATCH --job-name=moflow           # Name of the job
 #SBATCH --ntasks=1                  # Number of tasks (usually should be 1 for non-parallel jobs)
 #SBATCH --cpus-per-task=4           # Number of CPU cores per task
 #SBATCH --mem=250G                  # Memory needed (adjust if necessary)
-#SBATCH --time=2-00:00:00           # Expected runtime (max)
+#SBATCH --time=6-00:00:00           # Expected runtime (max)
 #SBATCH --output=result_%j.out      # Standard output and error log
 
 # Load necessary modules
@@ -17,23 +17,10 @@ module load anaconda3/2023.03-py3.10
 source ~/.bashrc
 conda activate moflow
 
-"""
-conda create --name moflow 
-conda activate moflow
-conda install pytorch torchvision -c pytorch 
-conda install -c conda-forge rdkit=2023.09.5 -n moflow
-conda install -c conda-forge orderedset
-conda install tabulate 
-conda install networkx 
-conda install scipy  
-conda install seaborn  
-pip install cairosvg
-pip install tqdm  """
-
 # Navigate to the data directory and preprocess the data
 cd moflow-master/data
 python data_preprocess.py --data_name zinc250k
 
 # Navigate to the mflow directory and run your machine learning training script
 cd ../mflow
-python train_model.py --data_name zinc250k  --batch_size 256  --max_epochs 200 --gpu 0  --debug True  --save_dir=results/qm9_64gnn_128-64lin_1-1mask_0d6noise_convlu1  --b_n_flow 10  --b_hidden_ch 128,128  --a_n_flow 27 --a_hidden_gnn 64  --a_hidden_lin 128,64  --mask_row_size_list 1 --mask_row_stride_list 1 --noise_scale 0.6 --b_conv_lu 1  2>&1 | tee qm9_64gnn_128-64lin_1-1mask_0d6noise_convlu1.log
+python train_model.py --data_name zinc250k  --batch_size 256  --max_epochs 50 --gpu 0  --debug True  --save_dir=results/qm9_64gnn_128-64lin_1-1mask_0d6noise_convlu1  --b_n_flow 10  --b_hidden_ch 128,128  --a_n_flow 27 --a_hidden_gnn 64  --a_hidden_lin 128,64  --mask_row_size_list 1 --mask_row_stride_list 1 --noise_scale 0.6 --b_conv_lu 1  2>&1 | tee qm9_64gnn_128-64lin_1-1mask_0d6noise_convlu1.log
